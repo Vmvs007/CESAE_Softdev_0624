16d66823c73c5c4bdaf29e4c516c2b4b3bf18893
Vitor Santos <vmvs007@gmail.com>
2024-06-28 09:55:39 +0100

    FichaPratica05

---
+++ FichasJava/src/FichaPratica05/Ex_01.java
@@ -0,0 +1,30 @@
+package FichaPratica05;
+
+import java.util.Scanner;
+
+public class Ex_01 {
+    public static void main(String[] args) {
+
+        // Import Scanner
+        Scanner input = new Scanner(System.in);
+
+        // Declarar variáveis
+        int[] vetor = new int[3];
+
+        // Ler vetor
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.print("Insira no Vetor[" + i + "]: ");
+            vetor[i] = input.nextInt();
+
+        }
+
+        System.out.println("\n___________________________________________\n");
+
+        // Imprimir vetor
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("Vetor[" + i + "] = " + vetor[i]);
+        }
+
+
+    }
+}
