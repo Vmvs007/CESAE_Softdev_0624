5e898d557c4168d2acf9a425517632c4ae0c1eb3
Vitor Santos <vmvs007@gmail.com>
2024-07-02 14:49:42 +0100

    FichaPratica06

--- FichasJava/src/FichaPratica06/Ex_01.java
+++ FichasJava/src/FichaPratica06/Ex_01.java
@@ -1,4 +1,56 @@
 package FichaPratica06;
 
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Ex_01 {
+
+    public static void fazerBarulho(String animal) {
+
+        animal=animal.toLowerCase();
+
+        switch (animal) {
+            case "cao":
+            case "cão":
+                System.out.println("Au au au ");
+                break;
+
+            case "gato":
+                System.out.println("Miau");
+                break;
+
+            case "peixe":
+                System.out.println("Glub glub glub");
+                break;
+
+            case "vaca":
+                System.out.println("Muuuuu");
+                break;
+
+            case "porco":
+                System.out.println("Oinc oinc");
+                break;
+
+            default:
+                System.out.println("Barulho estranho");
+                break;
+        }
+    }
+
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        String animalEscolhido;
+
+        while (true) {
+            System.out.print("Introduz um animal: ");
+            animalEscolhido = input.next();
+
+            fazerBarulho(animalEscolhido);
+        }
+
+
+    }
 }
--- FichasJava/src/FichaPratica06/Ex_02.java
+++ FichasJava/src/FichaPratica06/Ex_02.java
@@ -1,4 +1,53 @@
 package FichaPratica06;
 
+import java.util.Scanner;
+
 public class Ex_02 {
+
+    /**
+     * Método que lê um número inteiro e positivo do user
+     *
+     * @return Número Inteiro e Positivo
+     */
+    public static int lerPositivoInteiro() {
+
+        Scanner input = new Scanner(System.in);
+
+        int numeroLido;
+
+        do {
+
+            System.out.print("Introduza um número inteiro: ");
+            numeroLido = input.nextInt();
+
+        } while (numeroLido < 0);
+
+        return numeroLido;
+    }
+
+    /**
+     * Método que imprime uma linha com x asteriscos
+     * @param quantidadeAsteriscos Quantidade de Asteriscos a serem impressos
+     */
+
+    public static void linhaAsteriscos(int quantidadeAsteriscos) {
+
+        for (int i = 0; i < quantidadeAsteriscos; i++) {
+            System.out.print("*");
+        }
+        System.out.println();
+
+    }
+
+
+    public static void main(String[] args) {
+
+       //  linhaAsteriscos(30);
+        int numeroUser = lerPositivoInteiro();
+        linhaAsteriscos(numeroUser);
+
+       //  linhaAsteriscos(30);
+
+    }
+
 }
